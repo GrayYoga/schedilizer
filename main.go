572a906e13367ff5e6bcc3c062b186bd1a893262
Sergey Boiko <s.boiko@rogii.com>
2022-07-09 14:55:26 +0400

    Задание на 20 баллов

--- main.go
+++ main.go
@@ -1,35 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"log"
-	"os"
+	"schedulizer/loader"
 	"schedulizer/runner"
-	"schedulizer/types"
-	"time"
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+	tasks, err := loader.LoadTasks("input.txt")
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-
-		}
-	}(file)
-	var tasks []types.Task
-	num := 0
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		str := scanner.Text()
-		dur, _ := time.ParseDuration(str)
-		tasks = append(tasks, types.Task{Num: num, Dur: dur})
-		num = num + 1
-	}
-	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 	runner.Run(tasks)
--- runner/runner.go
+++ runner/runner.go
@@ -3,15 +3,23 @@ package runner
 import (
 	"log"
 	"schedulizer/types"
+	"sync"
 	"time"
 )
 
+var wg sync.WaitGroup
+
 func Run(tasks []types.Task) {
+
 	for _, t := range tasks {
-		task(t)
+		wg.Add(1)
+		locT := t
+		go task(locT)
 	}
+	wg.Wait()
 }
 func task(task types.Task) {
+	defer wg.Done()
 	log.Printf("Start task #%d duration: %v\n", task.Num, task.Dur)
 	time.Sleep(task.Dur)
 	log.Printf("Stop task  #%d\n", task.Num)
